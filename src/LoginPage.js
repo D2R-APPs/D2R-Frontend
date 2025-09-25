d515561121fe9af333a264d07c9a9c5bbe0d7023
Harsha vardhan Reddy <harshagondesi143@gmail.com>
2025-09-25 06:48:46 +0530

    Update LoginPage.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -11,7 +11,7 @@ const LoginPage = ({ onLoginSuccess }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('https://d2r-backend-83gd.onrender.com/api/auth/login', { email, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('userRole', res.data.user.role);
       onLoginSuccess(res.data.user.role);
